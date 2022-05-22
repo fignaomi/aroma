fbb94474480709190a483247f48d2d8d84bfdf12
fignaomi <naomi.figueiredo@gmail.com>
2022-05-22 14:28:18 -0300

    bd aws

--- backend/config/dbServer.js
+++ backend/config/dbServer.js
@@ -1,9 +1,9 @@
 let mysql = require('mysql');
 module.exports = function () {
     return connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '1234',
+        host: 'mydb.chw2xg6yedfk.us-east-1.rds.amazonaws.com',
+        user: 'admin',
+        password: 'tangerina',
         database: 'aroma'
     })
 };
